b735990fe0f97e6482fdd3f823235b984ef0a8f7
thedefidude18 <102023568+thedefidude18@users.noreply.github.com>
2023-12-29 13:06:36 +0000

    Pending changes exported from your codespace

---
+++ app-crm/src/routes/scrumboard/sales/edit-stage.tsx
@@ -0,0 +1,51 @@
+import { useModalForm } from "@refinedev/antd";
+import { useInvalidate, useNavigation } from "@refinedev/core";
+
+import { Form, Input, Modal } from "antd";
+
+export const SalesEditStage = () => {
+    const invalidate = useInvalidate();
+    const { list } = useNavigation();
+
+    const { formProps, modalProps, close } = useModalForm({
+        action: "edit",
+        defaultVisible: true,
+        resource: "dealStages",
+        meta: {
+            fields: ["id", "title"],
+        },
+        onMutationSuccess: () => {
+            invalidate({ invalidates: ["list"], resource: "deals" });
+        },
+        successNotification: () => {
+            return {
+                key: "edit-stage",
+                type: "success",
+                message: "Successfully updated stage",
+                description: "Successful",
+            };
+        },
+    });
+
+    return (
+        <Modal
+            {...modalProps}
+            onCancel={() => {
+                list("deals", "replace");
+                close();
+            }}
+            title="Edit stage"
+            width={512}
+        >
+            <Form {...formProps} layout="vertical">
+                <Form.Item
+                    label="Title"
+                    name="title"
+                    rules={[{ required: true }]}
+                >
+                    <Input />
+                </Form.Item>
+            </Form>
+        </Modal>
+    );
+};
